a2b3d80ea804e15fd87c4e903fe7b9faef57f9ca
onion <979325848@qq.com>
2017-03-02 14:56:55 +0800

    init

---
+++ promise-pop.js
@@ -0,0 +1,79 @@
+// created by dcy 2017-03-01
+
+const POP3Client = require("poplib")
+
+exports.CreateClient = CreateClient
+
+function errorHandler(client, reject) {
+    function onError(err) {
+        if (err.errno === 111) err = "Unable to connect to server"
+        else err = "Server error occurred: " + err.errno
+        reject(new Error(err))
+        client.quit()
+    }
+
+    function onInvalidState(cmd) {
+        let err = "Invalid state. You tried calling " + cmd
+        reject(new Error(err))
+        client.quit()
+    }
+
+    function onLocked(cmd) {
+        let err = "Current command has not finished yet. You tried calling " + cmd
+        reject(new Error(err))
+        client.quit()
+    }
+    client.on("error", onError)
+    client.on("invalid-state", onInvalidState)
+    client.on("locked", onLocked)
+    return function() {
+        client.removeListener('error', onError)
+        client.removeListener('invalid-state', onInvalidState)
+        client.removeListener('locked', onLocked)
+    }
+}
+
+function CreateClient() {
+    return new Promise((resolve, reject) => {
+        const client = new POP3Client(...arguments)
+        const cleanup = errorHandler(client, reject)
+        client.on('connect', () => {
+            cleanup()
+            resolve(new Client(client))
+        })
+    })
+}
+
+class Client {
+    constructor(client) {
+        this._client = client
+    }
+    login(...arg) {
+        return runMethod(this, 'login', ...arg)
+    }
+    stat() {
+        return runMethod(this, 'stat')
+    }
+    list(...arg) {
+        return runMethod(this, 'list', ...arg)
+    }
+    retr(...arg) {
+        return runMethod(this, 'retr', ...arg)
+    }
+    quit() {
+        return runMethod(this, 'quit')
+    }
+}
+
+function runMethod(clientWrapper, methodname, ...arg) {
+    return new Promise((resolve, reject) => {
+        (clientWrapper._client[methodname])(...arg)
+        let cleanup = errorHandler(clientWrapper._client, reject)
+        clientWrapper._client.on(methodname, (...rest) => {
+            cleanup()
+            resolve(rest)
+        })
+    })
+}
+
+
---
+++ test.js
@@ -0,0 +1,24 @@
+const { CreateClient } = require('./promise-pop')
+async function run() {
+    const userProfile = {
+        // recent模式，获取30天内全部的邮件信息
+        mail: 'recent:chongyang1994@gmail.com',
+        password: 'memeda'
+    }
+
+    const port = 995
+    const host = 'pop.gmail.com'
+    const client = await CreateClient(port, host, {
+        tlserrs: false,
+        enabletls: true,
+        debug: false
+    })
+    let res = await client.login(userProfile.mail, userProfile.password)
+    let list = await client.list()
+    for (let id of [...Array(list[1]).keys()].reverse()) {
+        let reponse = await client.retr(id + 1)
+        console.log(reponse[2]);
+        await sleep(100000)
+    }
+}
+run()
